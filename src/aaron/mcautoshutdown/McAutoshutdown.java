d1603bc52b9ae1ffa6744f48f95b73e58a115720
Aaron <kyrumx@hotmail.nl>
2021-01-28 14:47:36 +0100

    Added javadoc

--- src/aaron/mcautoshutdown/McAutoshutdown.java
+++ src/aaron/mcautoshutdown/McAutoshutdown.java
@@ -2,7 +2,6 @@ package aaron.mcautoshutdown;
 
 import aaron.mcautoshutdown.listener.EmptyServerListener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public class McAutoshutdown extends JavaPlugin {
     @Override
--- src/aaron/mcautoshutdown/listener/EmptyServerListener.java
+++ src/aaron/mcautoshutdown/listener/EmptyServerListener.java
@@ -1,36 +1,51 @@
 package aaron.mcautoshutdown.listener;
 
 import aaron.mcautoshutdown.McAutoshutdown;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public class EmptyServerListener implements Listener {
 
     private int schedulerId = -1;
     private final McAutoshutdown plugin;
+    private BukkitScheduler scheduler;
 
     public EmptyServerListener(McAutoshutdown plugin) {
         this.plugin = plugin;
+        this.scheduler = plugin.getServer().getScheduler();
     }
 
+    /**
+     The quit event handler, queues a task if the server becomes empty after the player has left
+     @param event The player quit event
+     */
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        if(plugin.getServer().getOnlinePlayers().size() <= 0) {
-            this.schedulerId = plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable() {
+        // Need to substract one since the server still counts the player as online while in the quit event
+        if(plugin.getServer().getOnlinePlayers().size() - 1 <= 0) {
+            Bukkit.broadcastMessage("Starting shutdown timer....");
+            this.schedulerId = this.scheduler.scheduleSyncDelayedTask(this.plugin, new Runnable() {
                 @Override
                 public void run() {
                     plugin.getServer().shutdown();
                 }
-            }, 6000);
+            }, 6000L);
         }
     }
 
+    /**
+     The quit event handler, cancels the shutdown task if it had been queued
+     @param event The player join event
+     */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (this.schedulerId != -1) {
-            plugin.getServer().getScheduler().cancelTask(schedulerId);
+            Bukkit.broadcastMessage("Cancelled shutdown timer!");
+            this.scheduler.cancelTask(schedulerId);
             this.schedulerId = -1;
         }
     }
